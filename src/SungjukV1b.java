4c962eac62089d3cf7322d9587960f3cca673511
서풍운 <neoseokorea@gmail.com>
2021-03-17 17:32:04 +0900

    키보드 입력이 가능해졌습니다.

---
+++ src/SungjukV1b.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author poong
+ * @category javabasic
+ * @version 1.0 자바프로그램 기초
+ *
+ *          자바 기초 프로그램 - 성적처리 프로그램 입력 : 이름, 국어, 영어, 수학 처리 : 총점, 평균, 학점 출력 : 이름,
+ *          국어, 영어, 수학, 총점, 평균, 학점
+ * 
+ *          이름과 성적데이터를 키보드로 직접 입력받아 처리 처리 결과 출력시 printf 메서드를 이용ㅇ *
+ */
+public class SungjukV1b {
+	public static void main(String[] args) {
+		// 변수 선언
+		String name; // 이름
+		int kor;
+		int eng;
+		int math;
+		int total;
+		double avg;
+		char grd;
+
+		// 처리
+		Scanner sc = new Scanner(System.in);
+		// 키보드를 이용해서 성적데이터를 입력받기 위해 Scanner 클래스를 초기화함.
+
+		System.out.println("이름은?");
+		name = sc.next(); // 키보드로 문자데이터를 입력받아 name 변수에 대입.
+		System.out.println("국어점수는?");
+		kor = sc.nextInt();
+		System.out.println("영어점수는?");
+		eng = sc.nextInt();
+		System.out.println("수학점수는?");
+		math = sc.nextInt();
+		total = kor + eng + math;
+		avg = total / 3;
+		grd = '우';
+
+		// 출력
+		System.out.println("------- 결과 -------");
+		System.out.println("이름: " + name);
+		System.out.println("국어: " + kor);
+		System.out.println("영어: " + eng);
+		System.out.println("수학: " + math);
+		System.out.println("총합: " + total);
+		System.out.println("평균: " + avg);
+		System.out.println("학점: " + grd);
+	}
+}
